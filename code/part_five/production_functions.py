2fc866703d54f8562149f82f06dfe5dae2aa213e
Herr.01001000 <s6wehuuu@uni-bonn.de>
2018-11-15 18:17:14 +0100

    Add the ability to recognize the situation when factors and weights do not have the same length to the all functions.

--- code/part_five/production_functions.py
+++ code/part_five/production_functions.py
@@ -5,24 +5,32 @@
 def general_cobb_douglas(factors, weights, a):
     """Return the value of a general Cobb-Douglas function with
 	inputs *factors* and technology parameters *a* and *weights*.
+    The list *factors* and *weights* should have the same length.
 	"""
-    output = a
-    for i in range(len(factors)):
-        output = output * factors[i] ** weights[i]
+    if len(factors) == len(weights):
+        output = a
+        for i in range(len(factors)):
+            output = output * factors[i] ** weights[i]
         
-    return output
+        return output
+    else:
+        print("Error: Factors and weights don't have the same length.")
 
 def general_ces(factors, weights, a, rho):
     """Return the value of a Constant Elasticity of Subsititution 
     production function with inputs *factors* and technology parameters
-    *a* and *weights*.
+    *a* and *weights*. The list *factors* and *weights* should have 
+    the same length.
 	"""
-    output = 0
-    for i in range(len(factors)):
-        output = output + weights[i]*factors[i]**(-rho)
-        
-    output = a * output**(-1/rho)
-    return output
+    if len(factors) == len(weights):
+        output = 0
+        for i in range(len(factors)):
+            output = output + weights[i]*factors[i]**(-rho)
+            
+        output = a * output**(-1/rho)
+        return output
+    else:
+        print("Error: Factors and weights don't have the same length.")
 
 def robust_general_ces(factors, weights, a, rho):
     """Return the value of a Constant Elasticity of Subsititution 
@@ -31,11 +39,15 @@ def robust_general_ces(factors, weights, a, rho):
     will return the value of a general Cobb-Douglas function with 
     inputs *factors* and technology parameters *a* and *weights*, because
     when *rho* approaches zero, the output of the CES function is close to
-    the output of the Cobb-Douglas function.
+    the output of the Cobb-Douglas function. The list *factors* and *weights* 
+    should have the same length.
 	"""
-    if rho == 0:
-        output = general_cobb_douglas(factors, weights, a)
-        return output
+    if len(factors) == len(weights):
+        if rho == 0:
+            output = general_cobb_douglas(factors, weights, a)
+            return output
+        else:
+            output = general_ces(factors, weights, a, rho)
+            return output
     else:
-        output = general_ces(factors, weights, a, rho)
-        return output
+        print("Error: Factors and weights don't have the same length.")
